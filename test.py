9e697c0b6e92877a53799243ed9e5c91bf32356e
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-10-29 03:05:49 -0400

    Parse multiple statements

--- test.py
+++ test.py
@@ -3,8 +3,9 @@ from token import Token
 from parser import Parser
 
 
-tokens = [Token('print', ''), Token('left_paren', ''), Token('string', 'Hello World'), 
-          Token('right_paren', ''), Token('EOF', '')]
+tokens = [Token('print', ''), Token('left_paren', ''), Token('string', 'Hello, World!'), 
+          Token('right_paren', ''), Token('print', ''), Token('left_paren', ''), 
+          Token('string', 'I love Python!'), Token('right_paren', ''), Token('EOF', '')]
 parser = Parser(tokens=tokens)
 
 ast = parser.parse()
